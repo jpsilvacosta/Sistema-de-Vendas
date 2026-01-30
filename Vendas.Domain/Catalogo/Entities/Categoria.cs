6e632f3fb665c8b7f7480cf834af68cae76882ae
João Pedro Costa <jpsilvacosta1807@outlook.com>
2026-01-30 15:47:19 -0300

    Criação BC Catalogo, junto à entity Categoria e aos Events do BC

---
+++ Vendas.Domain/Catalogo/Entities/Categoria.cs
@@ -0,0 +1,48 @@
+﻿using Vendas.Domain.Catalogo.Events;
+using Vendas.Domain.Common.Base;
+using Vendas.Domain.Common.Exceptions;
+using Vendas.Domain.Common.Validations;
+
+namespace Vendas.Domain.Catalogo.Entities
+{
+    public sealed class Categoria : AggregateRoot
+    {
+        public string Nome { get; private set; }
+        public string? Descricao { get; private set; }
+        public bool Ativa {  get; private set; }
+
+        public Categoria(string nome, string? descricao = null)
+        {
+            Guard.AgainstNullOrWhiteSpace(nome, nameof(nome), "Nome é obrigatório.");
+            Guard.Against<DomainException>(nome.Length < 3, "Nome deve ter ao menos 3 caracteres");
+
+            Nome = nome.Trim();
+            Descricao = descricao;
+            Ativa = true;
+        }
+
+        public void Ativar()
+        {
+            Guard.Against<DomainException>(Ativa, "Categoria já está ativa.");
+
+            Ativa = true;
+            SetDataAtualizacao();
+            AddDomainEvent(new CategoriaAtivadaEvent(Id));
+        }
+
+        public void Desativar()
+        {
+            Guard.Against<DomainException>(!Ativa, "Categoria já está inativa.");
+
+            Ativa = false;
+            SetDataAtualizacao();
+            AddDomainEvent(new CategoriaInativadaEvent(Id));
+        }
+
+        public void AlterarDescricao(string? novaDescricao)
+        {
+            Descricao = novaDescricao;
+            SetDataAtualizacao();
+        }
+    }
+}
---
+++ Vendas.Domain/Catalogo/Events/CategoriaInativadaEvent.cs
@@ -0,0 +1,6 @@
+﻿using Vendas.Domain.Common.Base;
+
+namespace Vendas.Domain.Catalogo.Events
+{
+    public sealed record CategoriaInativadaEvent(Guid CategoriaId) : DomainEventBase;
+}
