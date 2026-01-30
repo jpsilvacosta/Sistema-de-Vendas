e4a3df6c8358040d405b02add820935b2a49e326
João Pedro Costa <jpsilvacosta1807@outlook.com>
2026-01-30 08:55:27 -0300

    Criação dos DomainEvents do BC Clientes

---
+++ Vendas.Domain/Clientes/Events/ClienteCadastradoEvent.cs
@@ -0,0 +1,11 @@
+﻿using Vendas.Domain.Common.Base;
+
+namespace Vendas.Domain.Clientes.Events
+{
+    public sealed record ClienteCadastradoEvent(
+        Guid ClienteId,
+        string Nome,
+        string Cpf,
+        string Email
+        ) : DomainEventBase;
+}
---
+++ Vendas.Domain/Clientes/Events/EnderecoPrincipalAlteradoEvent.cs
@@ -0,0 +1,6 @@
+﻿using Vendas.Domain.Common.Base;
+
+namespace Vendas.Domain.Clientes.Events
+{
+    public sealed record EnderecoPrincipalAlteradoEvent(Guid ClienteId, Guid NovoEnderecoId) : DomainEventBase;
+}
