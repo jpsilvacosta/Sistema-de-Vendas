f0ead7da600929669687deba79c5a992bd118d0b
João Pedro Costa <jpsilvacosta1807@outlook.com>
2026-02-02 12:53:38 -0300

    Criação da entidade Produto e seus métodos, e também os eventos associados.

---
+++ Vendas.Domain/Catalogo/ValueObjects/CodigoProduto.cs
@@ -0,0 +1,24 @@
+﻿using Vendas.Domain.Common.Base;
+using Vendas.Domain.Common.Exceptions;
+using Vendas.Domain.Common.Validations;
+
+namespace Vendas.Domain.Catalogo.ValueObjects
+{
+    public sealed class CodigoProduto : ValueObject
+    {
+        public string Valor { get; }
+
+        public CodigoProduto(string valor)
+        {
+            Guard.AgainstNullOrWhiteSpace(valor, nameof(valor), "O código do produto é obrigatório.");
+            Guard.Against<DomainException>(valor.Length < 3, "O código do produto deve ter ao menos 3 caracteres.");
+
+            Valor = valor.Trim();
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Valor;
+        }
+    }
+}
