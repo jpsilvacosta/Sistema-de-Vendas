450ab29346a35de9ea3e4bfe074abb63e8980ea2
João Pedro Costa <jpsilvacosta1807@outlook.com>
2026-02-05 08:26:05 -0300

    Criação de commands, handlers e DTOs de Pedido

---
+++ Vendas.Application/Commands/Pedidos/CriarPedido/CriarPedidoResultDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Vendas.Application.Commands.Pedidos.CriarPedido
+{
+    public sealed class CriarPedidoResultDto
+    {
+        public Guid PedidoId { get; }
+        public string NumeroPedido { get; }
+        public DateTime DataCriacao { get; }
+        public decimal ValorTotal { get; }
+        public string Status {  get; }
+
+        public CriarPedidoResultDto(Guid pedidoId, string numeroPedido, DateTime dataCriacao, decimal valorTotal
+                                    string status)
+        {
+            PedidoId = pedidoId;
+            NumeroPedido = numeroPedido;
+            DataCriacao = dataCriacao;
+            ValorTotal = valorTotal;
+            Status = status;
+        }
+    }
+}
