fdf9e078ee48397aa4f75b6cd8c592088caa36fa
João Pedro <jpsilvacosta1807@gmail.com>
2026-03-02 14:34:23 -0300

    Recriação dos Enums

---
+++ Vendas.Domain/Common/Enums/MetodoPagamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vendas.Domain.Common.Enums
+{
+    public enum MetodoPagamento
+    {
+        CartaoCredito = 1,
+        CartaoDebito = 2,
+        Pix = 3,
+        Boleto = 4,
+        TransferenciaBancaria = 5
+    }
+}
---
+++ Vendas.Domain/Common/Enums/StatusPedido.cs
@@ -0,0 +1,12 @@
+﻿namespace Vendas.Domain.Common.Enums
+{
+    public enum StatusPedido
+    {
+        Criado = 1,
+        Pago = 2,
+        Cancelado = 3,
+        Enviado = 4,
+        Entregue = 5,
+        Concluido = 6
+    }
+}
