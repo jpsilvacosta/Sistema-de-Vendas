9221bf59762125b1801597ac8bc7630513e59e0b
João Pedro <joao.costa@meucurso.com.br>
2026-03-16 12:25:39 -0300

    Inclusão de classes fakes que simulam um sistema real com Gateways e Repositories

---
+++ Vendas.Infra/Fakes/FakeCatalogoGateway.cs
@@ -0,0 +1,21 @@
+﻿using Vendas.Domain.Pedidos.Integration.Catalogo;
+
+namespace Vendas.Infra.Fakes
+{
+    public sealed class FakeCatalogoGateway : ICatalogoGateway
+    {
+        private static readonly Dictionary<Guid, ProdutoDto> _produtos = new()
+        {
+            [new Guid("11111111-0000-0000-0000-000000000001")]  = new(new Guid("11111111-0000-0000-0000-000000000001"), "Notebook Gamer", 8500.00m),
+            [new Guid("11111111-0000-0000-0000-000000000002")]  = new(new Guid("11111111-0000-0000-0000-000000000002"), "Smartphone", 2500.00m),
+            [new Guid("11111111-0000-0000-0000-000000000003")]  = new(new Guid("11111111-0000-0000-0000-000000000003"), "Headset", 300.00m),
+            [new Guid("11111111-0000-0000-0000-000000000004")]  = new(new Guid("11111111-0000-0000-0000-000000000004"), "Teclado Mecânico", 450.00m),
+        };
+
+        public Task<ProdutoDto?> ObterProdutoPorIdAsync(Guid produtoId, CancellationToken ct = default)
+        {
+            _produtos.TryGetValue(produtoId, out var produto);
+            return Task.FromResult(produto);
+        }
+    }
+}
